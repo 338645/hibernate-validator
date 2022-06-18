af126902057e650d0bbc6a3bad6972f12460e670
su1jia1le19 <1525409228@qq.com>
2022-06-18 16:45:39 +0800

    2022-6-18 16:45

---
+++ src/main/java/com/mystudy/hibernatevalidation/HibernateValidationApplication.java
@@ -0,0 +1,13 @@
+package com.mystudy.hibernatevalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mystudy/hibernatevalidation/HibernateValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystudy.hibernatevalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
